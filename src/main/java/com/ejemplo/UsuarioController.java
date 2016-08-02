3092bc5002b6dd589312197325c0593a7fcc51db
Jose Rosario villanueva Morales <Chay@MacBook-Pro-de-Jose.local>
2016-08-01 22:19:35 -0500

    Commit inicial, Comienzo del proyecto.

---
+++ src/main/java/com/ejemplo/UsuarioController.java
@@ -0,0 +1,29 @@
+package com.ejemplo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsuarioController {
+
+	
+
+
+@Autowired
+UsuarioRepository dao;
+	
+	
+@RequestMapping("/inicio")
+public String inicio()
+{
+	UsuatioDTO usuario = new UsuatioDTO();
+	usuario.setEdad(1);
+	usuario.setId(123);
+	usuario.setNombre("Jose ROsario");
+	dao.save(usuario);
+	
+	
+	return "inicio";
+}
+}
---
+++ src/main/java/com/ejemplo/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.ejemplo;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<UsuatioDTO, Serializable>{
+
+}
